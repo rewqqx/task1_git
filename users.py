4c6a284c50ebd6e494d701305aebd2afc1893c06
rewqqx <rewqqx@gmail.com>
2023-03-22 17:24:01 +0300

    second change

--- users.py
+++ users.py
@@ -42,35 +42,3 @@ def get(users_service: UsersService = Depends(), user_id: int = Depends(get_curr
     print(user_id)
     return users_service.all()
 
-
-@router.get('/get/{user_id}', response_model=UserResponse, name='Получить одного пользователя')
-def get(user_id: int, users_service: UsersService = Depends(), user_req: int = Depends(check_admin)):
-    """
-    Получить одного пользователя
-    """
-    return get_with_check(user_id, users_service)
-
-
-def get_with_check(user_id: int, users_service: UsersService):
-    result = users_service.get(user_id)
-    if not result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Продукт не найден')
-    return result
-
-
-@router.put('/{user_id}', response_model=UserResponse, name="Обновить информацию о пользователе")
-def put_add(user_id: int, user_schema: UserRequest, users_service: UsersService = Depends(), user_req: int = Depends(check_admin)):
-    """
-    Обновить информацию обо мне
-    """
-    get_with_check(user_id, users_service)
-    return users_service.update(user_req, user_id, user_schema)
-
-
-@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT, name='Удалить пользователя')
-def delete(user_id: int, users_service: UsersService = Depends(), user_req: int = Depends(check_admin)):
-    """
-    Удалить пользователя
-    """
-    get_with_check(user_id, users_service)
-    return users_service.delete(user_id)
